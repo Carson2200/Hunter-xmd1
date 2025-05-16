f08b5a2976ea560ca5333b11b13a95ebb4ae9d8f
carson carson <123073549+Carson2200@users.noreply.github.com>
2025-05-16 13:01:11 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "HUNTER-XMD~TMIWgYLQ#CNwxvtVUJ5u-BKvTCY-o9Uen6LDn7E6Sm7JTbE8TX-o",
+SESSION_ID: process.env.SESSION_ID || "HUNTER-XMD~3ZwS0AQT#J0MK0So8BX4eofIKPylZcKvRACczRqA-kBprvV9-JR4",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
@@ -29,7 +29,7 @@ PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
 BOT_NAME: process.env.BOT_NAME || "HUNTER-XMD",
 // add bot namw here for menu
-STICKER_NAME: process.env.STICKER_NAME || "HUNTER-XMD",
+STICKER_NAME: process.env.STICKER_NAME || "chasing the bag",
 // type sticker pack name 
 CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
 // make this true for custum emoji react    
@@ -37,7 +37,7 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "254701082940",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "254793928332",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "Obed TechX",
 // add bot owner name
@@ -71,12 +71,12 @@ AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "254791637868",
+DEV: process.env.DEV || "254793928332",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
 ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "log", 
 // change it to 'same' if you want to resend deleted message in same chat 
-AUTO_RECORDING: process.env.AUTO_RECORDING || "false"
+AUTO_RECORDING: process.env.AUTO_RECORDING || "true"
 // make it true for auto recoding 
 };
